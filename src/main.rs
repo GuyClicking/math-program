85f82b1de7bbffe67757057b3c00fb9ddea12350
Benjamin Paul <bpaul@bpaul.xyz>
2022-07-25 19:18:49 +1000

    Add an expression to LaTeX converter

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ impl Add for Expr {
                 v.push(rhs);
                 // TODO simplify equation
                 v
-            },
+            }
             _ => {
                 vec![self, rhs]
             }
@@ -43,7 +43,7 @@ impl Mul for Expr {
                 v.push(rhs);
                 // TODO simplify expression
                 v
-            },
+            }
             _ => {
                 vec![self, rhs]
             }
@@ -57,7 +57,7 @@ impl Neg for Expr {
     fn neg(self) -> Self {
         match self {
             Expr::Neg(e) => *e,
-            _ => Expr::Neg(Box::new(self))
+            _ => Expr::Neg(Box::new(self)),
         }
     }
 }
@@ -93,8 +93,61 @@ assigning_operator!(MulAssign, mul_assign, *);
 assigning_operator!(SubAssign, sub_assign, -);
 assigning_operator!(DivAssign, div_assign, /);
 
+impl Expr {
+    /// Write an expression as a latex math equation.
+    pub fn to_latex(&self) -> String {
+        match self {
+            Expr::Const(n) => n.to_string(),
+            Expr::X => "x".to_string(),
+            Expr::Neg(e) => format!("-{}", e.to_latex()),
+            Expr::Recip(e) => format!("\\frac{{1}}{{{}}}", e.to_latex()),
+            Expr::Sum(v) => {
+                let mut str = v[0].to_latex();
+                for e in v.iter().skip(1) {
+                    if let Expr::Neg(e) = e {
+                        str += "-";
+                        str += &e.to_latex();
+                    } else {
+                        str += "+";
+                        str += &e.to_latex();
+                    }
+                }
+                str
+            }
+            Expr::Prod(v) => {
+                let mut str = v[0].to_latex();
+                for e in v.iter().skip(1) {
+                    if let Expr::Sum(_) = e {
+                        str += "(";
+                        str += &e.to_latex();
+                        str += ")";
+                    } else {
+                        str += &e.to_latex();
+                    }
+                }
+                str
+            }
+        }
+    }
+}
+
 fn main() {
     let mut e = Expr::X;
-    e -= Expr::X - Expr::X;
+    e /= Expr::X - Expr::X;
+    e *= Expr::X + Expr::X;
+    println!("{}", e.to_latex());
     println!("{:?}", e);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn latex() {
+        let mut e = Expr::X;
+        e += Expr::Const(5) * Expr::X;
+        e /= Expr::X;
+
+        assert_eq!(e.to_latex(), "x+5x\\frac{1}{x}");
+    }
 }
