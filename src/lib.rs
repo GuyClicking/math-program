ae11704bd2c7b33e34f81d767138a0874cd61d35
Benjamin Paul <bpaul@bpaul.xyz>
2022-07-26 03:07:27 +1000

    more simplification but none of this is tested yet (not good!)

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ type Num = isize;
 
 /// An expression type! All mathematical expressions should be able to be expressed with this type.
 /// This type is essentially an AST (abstract syntax tree).
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum Expr {
     /// A constant value (e.g. 1, 6, 15)
     Const(Num),
@@ -31,7 +31,81 @@ pub enum Expr {
 
 impl Expr {
     fn like_terms_with(&self, term: &Expr) -> bool {
-        todo!()
+        if self == term {
+            return true;
+        }
+        match self {
+            Expr::Prod(a) => match term {
+                Expr::Prod(b) => {
+                    // If both terms, with the coefficients removed, are equal (after sorting) then
+                    // they are like terms!
+                    let mut a = a.clone();
+                    let mut b = b.clone();
+                    a.retain(|x| !matches!(x, Expr::Const(_)));
+                    a.sort();
+                    b.retain(|x| !matches!(x, Expr::Const(_)));
+                    b.sort();
+
+                    a == b
+                }
+                _ => todo!(),
+            },
+            Expr::Sum(a) => match term {
+                Expr::Sum(b) => {
+                    // If they are both sums and they have the same elements but in different orders, return
+                    // true
+                    // Sort first so that they have the same orders
+                    let mut a = a.clone();
+                    let mut b = b.clone();
+                    a.sort();
+                    b.sort();
+
+                    a == b
+                }
+                _ => false,
+            },
+            _ => false,
+        }
+    }
+    fn add_like_term(&mut self, term: &Expr) {
+        match self {
+            Expr::Const(a) => {
+                // If both terms are like terms and one is constant the other one must also be
+                // constant, so there doesn't need to be an else contrition here.
+                if let Expr::Const(b) = term {
+                    *self = Expr::Const(*a + *b);
+                }
+            }
+            Expr::Prod(a) => match term {
+                Expr::Prod(b) => {
+                    // Surely theres a better way than the into_iter filter closure next thing
+                    let c1 = a.iter_mut().find(|x| matches!(x, Expr::Const(_)));
+                    let c2 = b.iter().find(|x| matches!(x, Expr::Const(_)));
+                    if let Some(Expr::Const(c1)) = c1 {
+                        if let Some(Expr::Const(c2)) = c2 {
+                            *c1 += c2;
+                        }
+                    }
+                }
+                _ => {
+                    if a.contains(term) {
+                        // Ths case would just be like 3x + x for example
+                        // There is no coefficient product for the 2nd one
+                        // so add 1 to 3
+                        let c1 = a.iter_mut().find(|x| matches!(x, Expr::Const(_)));
+                        if let Some(Expr::Const(c1)) = c1 {
+                            *c1 += 1;
+                        }
+                    }
+                }
+            },
+            _ => {
+                if self == term {
+                    // This is just e + e = 2e
+                    *self = Expr::Prod(vec![Expr::Const(2), self.clone()]);
+                }
+            }
+        }
     }
 }
 
@@ -156,7 +230,10 @@ impl Expr {
     /// Apply all simplification techniques to an expression (INCOMPLETE!)
     ///
     /// List of applied simplifications:
+    /// [`Expr::simplify_singleton`]
     /// [`Expr::simplify_sums_in_sums`]
+    /// [`Expr::simplify_apply_sums`]
+    /// [`Expr::simplify_mult_consts`]
     pub fn simplify(&mut self) {
         match self {
             Expr::Sum(v) => {
@@ -165,11 +242,15 @@ impl Expr {
                     e.simplify();
                 }
 
+                if self.simplify_singleton() {
+                    return;
+                }
+
                 self.simplify_sums_in_sums();
 
                 // Turn addition into multiplication or something
                 // x + 2x = 3x for example
-                todo!();
+                self.simplify_apply_sums();
             }
             Expr::Prod(v) => {
                 // Simplify all of the terms
@@ -177,13 +258,48 @@ impl Expr {
                     e.simplify();
                 }
 
+                if self.simplify_singleton() {
+                    return;
+                }
+
                 // Cancel out fractions!
 
                 // Multiply constants
+                self.simplify_mult_consts();
                 // Also simplify fraction of constants
+                todo!();
             }
             _ => unimplemented!(),
         };
+    }
+
+    /// This function turns sums or products of singular terms into just their terms.
+    /// Returns true if the mutated term is no longer a multivalued operation (Sum or Prod)
+    pub fn simplify_singleton(&mut self) -> bool {
+        match self {
+            Expr::Sum(v) => {
+                if v.is_empty() {
+                    *self = Expr::Const(0);
+                    return true;
+                } else if v.len() == 1 {
+                    // I feel like I shouldn't use an unwrap but len == 1
+                    *self = v.first().unwrap().clone();
+                    return true;
+                }
+                false
+            }
+            Expr::Prod(v) => {
+                if v.is_empty() {
+                    *self = Expr::Const(0);
+                    return true;
+                } else if v.len() == 1 {
+                    *self = v.first().unwrap().clone();
+                    return true;
+                }
+                false
+            }
+            _ => false,
+        }
     }
 
     /// This function simplifies [`Expr::Sum`] expressions which have [`Expr::Sum`] values in their
@@ -219,9 +335,7 @@ impl Expr {
                 while j < i {
                     if v[i].like_terms_with(&v[j]) {
                         let e = v[i].clone();
-                        // Cant do this because it doesnt add the coefficients balbhalbalh
-                        v[j] += e;
-                        v[j].simplify();
+                        v[j].add_like_term(&e);
                         v.remove(i);
                         i -= 1;
                         break;
@@ -229,6 +343,29 @@ impl Expr {
                     j += 1;
                 }
                 i += 1;
+            }
+        }
+    }
+
+    /// This function multiplies constants in a product.
+    /// e.g. `5 * x * 6 = 30 * x`
+    pub fn simplify_mult_consts(&mut self) {
+        if let Expr::Prod(v) = self {
+            let mut i = 1;
+            while i < v.len() {
+                if let Expr::Const(b) = v[i] {
+                    let mut j = 0;
+                    while j < i {
+                        if let Expr::Const(a) = &mut v[j] {
+                            *a += b;
+                        }
+                        j += 1;
+                    }
+                    i += 1;
+                } else {
+                    i += 1;
+                    continue;
+                }
             }
         }
     }
