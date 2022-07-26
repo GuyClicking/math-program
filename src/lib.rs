f407fac7aec791a6c75abf5af5ca23a06b2a1f04
GuyClicking <bpaul@bpaul.xyz>
2022-07-26 15:16:57 +1000

    Did some work on my ipad yikes but simplificatoin is done

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ impl Expr {
 
                     a == b
                 }
-                _ => todo!(),
+                _ => a.contains(term),
             },
             Expr::Sum(a) => match term {
                 Expr::Sum(b) => {
@@ -62,9 +62,20 @@ impl Expr {
 
                     a == b
                 }
+                Expr::Prod(b) => b.contains(self),
                 _ => false,
             },
-            _ => false,
+            Expr::Const(_) => {
+                if let Expr::Const(_) = term {
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => match term {
+                Expr::Prod(b) => b.contains(self),
+                _ => self == term,
+            },
         }
     }
     fn add_like_term(&mut self, term: &Expr) {
@@ -105,6 +116,13 @@ impl Expr {
                     *self = Expr::Prod(vec![Expr::Const(2), self.clone()]);
                 }
             }
+        }
+    }
+
+    fn recip(self) -> Self {
+        match self {
+            Expr::Recip(e) => *e.clone(),
+            _ => Expr::Recip(Box::new(self.clone())),
         }
     }
 }
@@ -208,9 +226,18 @@ impl Expr {
                 str
             }
             Expr::Prod(v) => {
-                let mut str = v[0].to_latex();
+                let mut str = if v[0] == Expr::Const(1) {
+                    "".to_string()
+                } else {
+                    v[0].to_latex()
+                };
                 for e in v.iter().skip(1) {
                     if matches!(e, Expr::Sum(_)) || matches!(e, Expr::Const(_)) {
+                        if let Expr::Const(e) = e {
+                            if *e == 1 {
+                                continue;
+                            }
+                        }
                         str += "(";
                         str += &e.to_latex();
                         str += ")";
@@ -225,14 +252,13 @@ impl Expr {
 }
 
 impl Expr {
-    // TODO list all simplifications that this function applies
-    // TODO separate all of the simplifications into other functions
     /// Apply all simplification techniques to an expression (INCOMPLETE!)
     ///
     /// List of applied simplifications:
     /// [`Expr::simplify_singleton`]
     /// [`Expr::simplify_sums_in_sums`]
     /// [`Expr::simplify_apply_sums`]
+    /// [`Expr::simplify_cancel_fracs`]
     /// [`Expr::simplify_mult_consts`]
     pub fn simplify(&mut self) {
         match self {
@@ -242,15 +268,11 @@ impl Expr {
                     e.simplify();
                 }
 
-                if self.simplify_singleton() {
-                    return;
-                }
-
                 self.simplify_sums_in_sums();
 
-                // Turn addition into multiplication or something
-                // x + 2x = 3x for example
                 self.simplify_apply_sums();
+
+                self.simplify_singleton();
             }
             Expr::Prod(v) => {
                 // Simplify all of the terms
@@ -258,47 +280,47 @@ impl Expr {
                     e.simplify();
                 }
 
-                if self.simplify_singleton() {
-                    return;
-                }
-
                 // Cancel out fractions!
+                self.simplify_cancel_fracs();
 
                 // Multiply constants
                 self.simplify_mult_consts();
                 // Also simplify fraction of constants
-                todo!();
+
+                self.simplify_singleton();
             }
-            _ => unimplemented!(),
+            _ => (),
+        };
+        match self {
+            Expr::Sum(v) => {
+                v.sort();
+            }
+            Expr::Prod(v) => {
+                v.sort();
+            }
+            _ => (),
         };
     }
 
-    /// This function turns sums or products of singular terms into just their terms.
-    /// Returns true if the mutated term is no longer a multivalued operation (Sum or Prod)
-    pub fn simplify_singleton(&mut self) -> bool {
+    /// This function turns sums or products with a singular term into just their term.
+    pub fn simplify_singleton(&mut self) {
         match self {
             Expr::Sum(v) => {
                 if v.is_empty() {
                     *self = Expr::Const(0);
-                    return true;
                 } else if v.len() == 1 {
                     // I feel like I shouldn't use an unwrap but len == 1
                     *self = v.first().unwrap().clone();
-                    return true;
                 }
-                false
             }
             Expr::Prod(v) => {
                 if v.is_empty() {
                     *self = Expr::Const(0);
-                    return true;
                 } else if v.len() == 1 {
                     *self = v.first().unwrap().clone();
-                    return true;
                 }
-                false
             }
-            _ => false,
+            _ => (),
         }
     }
 
@@ -357,7 +379,10 @@ impl Expr {
                     let mut j = 0;
                     while j < i {
                         if let Expr::Const(a) = &mut v[j] {
-                            *a += b;
+                            *a *= b;
+                            v.remove(i);
+                            i -= 1;
+                            break;
                         }
                         j += 1;
                     }
@@ -366,6 +391,30 @@ impl Expr {
                     i += 1;
                     continue;
                 }
+            }
+        }
+    }
+
+    /// This fuction cancels out terms in a fraction
+    /// e.g. `5x/x = 5`
+    pub fn simplify_cancel_fracs(&mut self) {
+        if let Expr::Prod(v) = self {
+            let mut i = 0;
+            while i < v.len() {
+                let mut j = i + 1;
+                let mut inc = 1;
+                while j < v.len() {
+                    if v[i] == v[j].clone().recip() {
+                        v.remove(j);
+                        v.remove(i);
+                        v.push(Expr::Const(1));
+                        inc = 0;
+                        break;
+                    } else {
+                        j += 1;
+                    }
+                }
+                i += inc;
             }
         }
     }
@@ -381,5 +430,67 @@ mod tests {
         e /= Expr::X;
 
         assert_eq!(e.to_latex(), "x+x(5)\\frac{1}{x}");
+    }
+
+    #[test]
+    fn like_terms() {
+        let mut a = Expr::Prod(vec![Expr::Const(2), Expr::X]);
+        let b = Expr::X;
+
+        assert!(a.like_terms_with(&b));
+        assert!(b.like_terms_with(&a));
+
+        a.add_like_term(&b);
+        assert_eq!(a, Expr::Prod(vec![Expr::Const(3), Expr::X]));
+
+        let mut a = Expr::Const(2);
+        let b = Expr::Const(3);
+
+        assert!(a.like_terms_with(&b));
+
+        a.add_like_term(&b);
+        assert_eq!(a, Expr::Const(5));
+    }
+
+    #[test]
+    fn simplification() {
+        // Singleton test
+        let mut e = Expr::Sum(vec![Expr::X]);
+        e.simplify_singleton();
+        assert_eq!(e, Expr::X);
+
+        // Sums in sums
+        let mut e = Expr::X;
+        e += Expr::X + Expr::X;
+        e.simplify_sums_in_sums();
+        assert_eq!(e, Expr::Sum(vec![Expr::X, Expr::X, Expr::X]));
+
+        // Apply sums
+        let mut e = Expr::X + Expr::X + Expr::X;
+        e.simplify_apply_sums();
+        e.simplify_singleton();
+        assert_eq!(e, Expr::Prod(vec![Expr::Const(3), Expr::X]));
+
+        // Multiply consts
+        let mut e = Expr::Const(6) * Expr::X * Expr::Const(5);
+        e.simplify();
+        assert_eq!(e, Expr::Prod(vec![Expr::Const(30), Expr::X]));
+
+        // Fraction cancellation 1
+        let mut e = Expr::X / Expr::X;
+        e.simplify();
+        assert_eq!(e.to_latex(), "1");
+
+        // Fraction cancellation 2
+        let mut e = Expr::X * Expr::X / Expr::X;
+        e.simplify();
+        assert_eq!(e.to_latex(), "x");
+
+        // A bunch of stuff
+        let mut e = Expr::X;
+        e += Expr::Const(3) + Expr::Const(2);
+        e /= Expr::Const(5) + Expr::X;
+        e.simplify();
+        assert_eq!(e, Expr::Const(1));
     }
 }
